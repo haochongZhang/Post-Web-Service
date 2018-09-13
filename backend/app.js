31cb473906af0e7f309872e1be5fa1d58e225f14
hz132 <hczhang028@gmail.com>
2018-09-13 17:06:39 -0400

    add mongodb part2

--- backend/app.js
+++ backend/app.js
@@ -34,9 +34,11 @@ app.post('/api/posts', (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save();
-  res.status(201).json({
-    message: 'Post added successfully!'
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully!',
+      id: createdPost._id
+    });
   });
 });
 
@@ -49,6 +51,13 @@ app.get('/api/posts', (req, res, next) => {
         posts: documents
       });
     });
+});
+
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+  });
+  res.status(200).json({ message: 'Post deleted!'});
 });
 
 module.exports = app;
