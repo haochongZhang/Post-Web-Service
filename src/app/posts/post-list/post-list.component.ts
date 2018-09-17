3ac9fd5a48886d195e1e5df09dc1b98c1d31fbcd
hz132 <hczhang028@gmail.com>
2018-09-17 04:34:24 -0400

    user authentication part 2

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs';
 import { Post } from '../post.model';
 import { PostService } from '../posts.service';
 import { PageEvent } from '@angular/material';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-post-list',
@@ -18,14 +19,16 @@ export class PostListComponent implements OnInit, OnDestroy {
   ]; */
   posts: Post[] = [];
   private postsSub: Subscription;
+  private authStatusSub: Subscription;
   isLoading = false;
   totalPosts = 10;
   currentPage = 1;
   postsPerPage = 2;
   pageSizeOptions = [1, 2, 5, 10];
+  userIsAuthenticated = false;
 
   // The 'public' keyword will automatically add property to class
-  constructor(public postsService: PostService) {}
+  constructor(public postsService: PostService, private authService: AuthService ) {}
 
   ngOnInit() {
     this.isLoading = true;
@@ -39,6 +42,10 @@ export class PostListComponent implements OnInit, OnDestroy {
         this.posts = postData.posts;
         this.totalPosts = postData.postCount;
       });
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authStatusSub = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
   }
 
   onChangedPage(pageData: PageEvent) {
@@ -50,6 +57,7 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.postsSub.unsubscribe();
+    this.authStatusSub.unsubscribe();
   }
 
   onDelete(postId: string) {
